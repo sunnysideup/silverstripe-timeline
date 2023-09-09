1b3f67511abe884de5f7e03573c40d8fe23a3b3e
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2023-09-10 11:30:04 +1200

    BUILD

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 import TimelineBlock from './javascript/components/timeline-block'
 
+console.log('DOM fully loaded and parsed')
 window.addEventListener('DOMContentLoaded', event => {
   console.log('DOM fully loaded and parsed')
   TimelineBlock.init()
